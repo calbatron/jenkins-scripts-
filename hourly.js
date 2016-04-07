a12423b0b0da44ea0e8b0ee255b62c7f18c5b09a
calbatron <daniel.morrison@mac.com>
2016-04-07 17:41:59 +0100

    hourly

--- hourly.js
+++ hourly.js
@@ -1,16 +1,14 @@
 'use strict';
 
 var unirest = require('unirest');
-var AWS = require('aws-sdk');
-var ec2 = new AWS.EC2({"region":"eu-west-1"});
-
+var ip = '10.38.164.61';
 var id = 0;
 var index = 0;
 var interval = null;
 var line = 0;
 
 var tail = function() {
-    unirest.get('http://localhost:3000/hourly-comscore/tail/' + id + '/' + index)
+    unirest.get('http://' + ip + ':3000/hourly-comscore/tail/' + id + '/' + index)
     .end(function(result) {
         if (result.status === 200) {
             var last = result.body.length - 1;
@@ -42,24 +40,17 @@ var tail = function() {
     });
 };
 
-var filter = {"Filters": [{"Name":"tag:BBCComponent", "Values":["clockwork"]},{"Name":"instance-state-name", "Values":["running"]},{"Name":"tag:BBCEnvironment", "Values":['test']}]};
-ec2.describeInstances(filter, function(err, res) {
-    if (err) {
-        console.log(err);
-    } else if (res.Reservations.length > 0) {
-        console.log('connecting to http://' + res.Reservations[0].Instances[0].PrivateIpAddress +':3000/hourly-comscore/start');
-        unirest.get('http://' + res.Reservations[0].Instances[0].PrivateIpAddress +':3000/hourly-comscore/start')
-        .end(function(result) {
-            if (result.status === 200) {
-                id = result.body.id;
-                interval = setInterval(tail, 2000);
-            }
-        });
 
-    } else {
-        console.log('no clockwork instances found');
+unirest.get('http://' + ip +':3000/hourly-comscore/start')
+.end(function(result) {
+    if (result.status === 200) {
+        id = result.body.id;
+        interval = setInterval(tail, 2000);
     }
 });
+
+
+
 
 
 
